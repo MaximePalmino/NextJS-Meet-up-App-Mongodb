e82766440412938a2f815f5e77efb94a77aff6b4
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-04-01 11:41:51 +0200

    [ADD]GetStaticProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "83beffca02855433f1c1";
+/******/ 	var hotCurrentHash = "45188328b2ad1bbc1195";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,42 @@
 import MeetupList from '../components/meetups/MeetupList'
 import Layout from "../components/layout/Layout"
 
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'My first title',
+    image: 'https://images.unsplash.com/photo-1648571113523-d0c7ca902bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    adresse:' An addresse '
+  },
+  {
+    id: 'm2',
+    title: 'My second title',
+    image: 'https://images.unsplash.com/photo-1648571113523-d0c7ca902bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    adresse:' An addresse 02'
+  }
+]
 
-const HomePage = () => {
+const HomePage = (props) => {
 
-    const DUMMY_MEETUPS = [
-        {
-          id: 'm1',
-          title: 'My first title',
-          image: 'https://images.unsplash.com/photo-1648571113523-d0c7ca902bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-          adresse:' An addresse '
-        },
-        {
-          id: 'm2',
-          title: 'My second title',
-          image: 'https://images.unsplash.com/photo-1648571113523-d0c7ca902bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-          adresse:' An addresse 02'
-        }
-      ]
 
       
     return (
         <>
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
         </>
     )
 }
+
+// export async function getStaticProps() {
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     },
+//     revalidate: 5
+//   }
+
+// }
+
 
 export default HomePage
